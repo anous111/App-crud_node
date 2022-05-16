dc20181ca9eef39e2544e8f605d4502178d42f77
Anis <anissafia90@gmail.com>
2022-05-16 09:29:43 +0100

    first commit

---
+++ routes/stuff.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+const stuffCtrl = require('../controllers/stuff');
+router.post('/', auth,stuffCtrl.createThing );
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+
+module.exports = router;
